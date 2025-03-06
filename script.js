2361eef20daa1869b0995c0beef8e91ac6146e95
eden6644 <edenraiven@gmail.com>
2025-03-06 09:19:12 +0100

    fixed page overflow

--- script.js
+++ script.js
@@ -1,17 +1,38 @@
-// 
+// // 
+// // setTimeout(function() {
+// //     window.addEventListener('scroll', function() {
+// //         let scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+// //         if (scrollTop > lastScrollTop) {
+// //             navbar.style.top = '-100px'; // Adjust the value based on your navbar height
+// //         } else {
+// //             navbar.style.top = '0';
+// //         }
+// //         lastScrollTop = scrollTop;
+// //     });
+// // }, 1000);
+// let lastScrollTop = 0;
+// const navbar = document.querySelector('.navbar');
 
-let lastScrollTop = 0;
-const navbar = document.querySelector('.navbar');
+// window.addEventListener('scroll', function() {
+//     let scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+//     if (scrollTop > lastScrollTop) {
+//         this.setTimeout(function() {
+//             navbar.style.top = '-100px'; // Adjust the value based on your navbar height
+//         }, 5000);
+//     } else {
+//         navbar.style.top = '0';
+//     }
+//     lastScrollTop = scrollTop;
+// });
 
-window.addEventListener('scroll', function() {
-    let scrollTop = window.pageYOffset || document.documentElement.scrollTop;
-    if (scrollTop > lastScrollTop) {
-        navbar.style.top = '-100px'; // Adjust the value based on your navbar height
-    } else {
-        navbar.style.top = '0';
-    }
-    lastScrollTop = scrollTop;
-});
+// if i click to the body the nav back to normal
+
+    document.body.addEventListener('click', function(event) {
+        var navbarCollapse = document.querySelector('.navbar-collapse');
+        if (navbarCollapse.classList.contains('show')) {
+            navbarCollapse.classList.remove('show');
+        }
+    });
 
 // Contact us map
 
@@ -26,3 +47,24 @@ function initMap() {
         map: map
     });
 }
+
+// lien active en fonction de la div sur écran
+
+    window.addEventListener('scroll', function() {
+        const sections = document.querySelectorAll('section');
+        const navLinks = document.querySelectorAll('.nav-link');
+
+        sections.forEach((section, index) => {
+            const rect = section.getBoundingClientRect();
+            const id = section.getAttribute('id');
+
+            if (rect.top <= window.innerHeight / 2 && rect.bottom >= window.innerHeight / 2) {
+                navLinks.forEach((link) => {
+                    link.classList.remove('active');
+                    if (link.getAttribute('href').includes(id)) {
+                        link.classList.add('active');
+                    }
+                });
+            }
+        });
+    });
